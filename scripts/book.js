354f50f5d102b74787517ffe52b5a2c204c49a18
jason89521 <97ssps30212@gmail.com>
2021-10-01 18:37:04 +0800

    convert plain constructor to class in book.js

--- scripts/book.js
+++ scripts/book.js
@@ -4,16 +4,18 @@
  * @param {number} pages 
  * @param {boolean} readStatus
  */
-function Book(title, author, pages, readStatus, id) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readStatus = readStatus;
-    this.id = (id) ? id : (new Date()).getTime();
+class Book {
+    constructor(title, author, pages, readStatus, id) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus;
+        this.id = (id) ? id : (new Date()).getTime();
+    }
+    ToggleReadStatus() {
+        this.readStatus = !this.readStatus;
+    }
 }
 
-Book.prototype.ToggleReadStatus = function () {
-    this.readStatus = !this.readStatus;
-}
 
 export {Book};
