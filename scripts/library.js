c749fff0244d44daa31301da4c44e3212605abe8
jason89521 <97ssps30212@gmail.com>
2021-10-01 18:43:04 +0800

    convert plain construtor to class in library.js

--- scripts/library.js
+++ scripts/library.js
@@ -1,21 +1,23 @@
 import { Book } from "./book.js";
 
-function Library() {
-    this.books = [];
-    this.date = new Date();
-
-    /**
-     * @param {Book} book 
-     */
-    this.AddBook = function (book) {
-        this.books.push(book);
-        localStorage.setItem(book.id, JSON.stringify(book));
+class Library {
+    constructor() {
+        this.books = [];
+        this.date = new Date();
     }
 
     /**
-     * @param {number} id 
+     * @param {Book} book
      */
-    this.RemoveBook = function (id) {
+    AddBook = function (book) {
+        this.books.push(book);
+        localStorage.setItem(book.id, JSON.stringify(book));
+    };
+
+    /**
+     * @param {number} id
+     */
+    RemoveBook = function (id) {
         const toRemove = this.findBook(id);
 
         if (toRemove) {
@@ -25,40 +27,41 @@ function Library() {
                 this.books.splice(index, 1);
             }
         }
-    }
+    };
 
     /**
-     * @param {number} id 
+     * @param {number} id
      */
-    this.ChangeReadStatus = function (id) {
+    ChangeReadStatus = function (id) {
         const toChange = this.findBook(id);
 
-        if(toChange) {
+        if (toChange) {
             toChange.ToggleReadStatus();
             localStorage.setItem(id.toString(), JSON.stringify(toChange));
         }
-    }
+    };
 
-    this.LoadBooks = function () {
-        if (this.books.length > 0) return;
+    LoadBooks = function () {
+        if (this.books.length > 0)
+            return;
 
         let keys = Object.keys(localStorage);
 
-        for(let i = 0; i < keys.length; i++) {
+        for (let i = 0; i < keys.length; i++) {
             const bookJSON = JSON.parse(localStorage.getItem(keys[i]));
-            const book = new Book(bookJSON.title, 
-                bookJSON.author, bookJSON.pages, 
+            const book = new Book(bookJSON.title,
+                bookJSON.author, bookJSON.pages,
                 bookJSON.readStatus, bookJSON.id);
             this.books.push(book);
         }
-    }
+    };
 
-    this.findBook = function (id) {
+    findBook = function (id) {
         const target = this.books.find((book) => {
             return book.id === id;
         });
         return target;
-    }
+    };
 }
 
 export { Library }
